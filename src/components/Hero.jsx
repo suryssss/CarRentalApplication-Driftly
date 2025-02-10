25d4b66fbcda051bfe187860f3ca2d8875f22858
Rithwik Surya <surya.rithwik2005@gmail.com>
2025-02-10 14:39:42 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,6 @@ import Search from './Search'
 function Hero() {
   return (
         <div className='fixed top-20 left-0 flex flex-col items-center p-10 py-20 gap-6 h-[600px] w-full bg-[#eef0fc]'>
-          <img src='background.jpg'>
             <h2 className='text-lg font-bold'>Your Ultimate Solution For Car Rentals</h2>
             <h2 className='text-[60px] font-bold'>Find Your Dream Car</h2>
             <Search/>
